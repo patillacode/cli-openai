7fa17a54dac1f8292a9e175d0d8c58552ed98b65
PatillaCode <patillacode@gmail.com>
2023-11-23 19:10:12 +0100

    fix new openai audio handling

--- audio.py
+++ audio.py
@@ -20,11 +20,15 @@ def transcribe_audio(audio_file, verbose=False):
     try:
         console = Console()
         with console.status("[bold green]transcribing..."):
-            transcript = client.audio.transcribe("whisper-1", open(audio_file, "rb"))
+            transcript = client.audio.transcriptions.create(
+                file=open(audio_file, "rb"),
+                model="whisper-1",
+                response_format="text",
+            )
         display_output(
             "Below is the content of the given audio file in text form:\n", "yellow"
         )
-        display_output(f"{transcript.text}\n")
+        display_output(f"{transcript}\n")
     except Exception as e:
         handle_error(e, verbose)
 
@@ -40,11 +44,15 @@ def translate_audio(audio_file, verbose=False):
     try:
         console = Console()
         with console.status("[bold green]translating..."):
-            translation = client.audio.translate("whisper-1", open(audio_file, "rb"))
+            translation = client.audio.translations.create(
+                file=open(audio_file, "rb"),
+                model="whisper-1",
+                response_format="text",
+            )
         display_output(
             "Below is the translated content of the given audio file in text form:\n",
             "yellow",
         )
-        display_output(f"{translation.text}\n")
+        display_output(f"{translation}\n")
     except Exception as e:
         handle_error(e, verbose)
