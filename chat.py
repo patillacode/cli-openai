8d6eded049688ee1a0c57d3c8bb57d060c786b2b
PatillaCode <patillacode@gmail.com>
2023-11-29 02:07:49 +0100

    fix more new stuff

--- chat.py
+++ chat.py
@@ -20,25 +20,38 @@ def start_chat(model, verbose=False):
     display_output("Starting chat...")
 
     system = {"role": "system", "content": f"DIRECTIVE_FOR_{model}"}
-    message = {"role": "user", "content": ""}
+    # message = {"role": "user", "content": ""}
     conversation = [system]
     tokens = 0
     console = Console()
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=conversation,
+        # stream=True,
+    )
+    # for chunk in completion:
+    #     print(chunk.choices[0].delta)
+
     while True:
         prompt = input("\n􀳾 > ")
         if prompt == "exit":
             break
         try:
-            message["content"] = prompt
-            conversation.append(message)
+            conversation.append({"role": "user", "content": prompt})
             with console.status("[bold green]Generating..."):
                 completion = client.chat.completions.create(
-                    model=model, messages=conversation
+                    model=model,
+                    messages=conversation,
+                    stream=True,
                 )
-
+            # for chunk in completion:
+            #     if chunk.choices[0].delta.content is not None:
+            #         print(chunk.choices[0].delta.content, end="")
             message = completion.choices[0].message
-            conversation.append(message.content)
+            conversation.append({"role": "system", "content": message.content})
             tokens = completion.usage.total_tokens
+            ic(conversation)
             display_output(f"\n􀪬  > {message.content}")
             display_output(f"\n􀤚  ({tokens})", color="magenta")
 
